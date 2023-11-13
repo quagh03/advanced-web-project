d5fe57e188d385fb09da00fff045b5e215b6a1f2
quagh03 <03.quanghuy.le@gmail.com>
2023-11-13 15:56:24 +0700

    initial commit

---
+++ src/test/java/com/example/advancedwebproject/AdvancedWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancedwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
